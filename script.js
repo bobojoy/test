95087c0e65e0a150bbd1e38de690b6edaf15d2c6
Joy Wambui <joy.wambui@student.moringaschool.com>
2024-07-12 09:58:09 +0300

    Changed comments &removed unnecessary code

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   
+ //Function addToLIst
   function addToList(e) {
     e.preventDefault()
     const newItemInput = document.getElementById("itemInput");
 
     const newItem = newItemInput.value.trim();
 
-    //Returns an alert message when there is no input
+    
     if (!newItem) {
-      alert("Enter item to continue !");
+      alert();
       return;
     }
 
@@ -19,31 +20,24 @@ document.addEventListener("DOMContentLoaded", () => {
       this.classList.toggle("purchased");
     });
 
-    // Append the new list item and delete button to the existing list
+    
     const list = document.getElementById("itemList");
     list.appendChild(itemList);
 
     newItemInput.value = "";
+    
   }
-  // Function to add an item
-
+  
+//Form Submission Listener
   const form = document.getElementById("Shopping");
   form.addEventListener("submit", addToList);
 
-  
-
-  // Function to clear the entire list
+//Clear List Button
   const clearButton = document.getElementById("clearListButton");
   clearButton.addEventListener("click", function () {
     const itemListNew = document.getElementById("itemList");
-    itemListNew.innerHTML = ""; // Clear the list
+    itemList.innerHTML = ""; 
   });
-  const list = document.getElementById("itemList");
-  list.addEventListener("click", function (event) {
-    if (event.target.tagName === 'LI') {
-      event.target.classList.toggle('purchased');
-    } else if (event.target.tagName === 'BUTTON') {
-      event.target.parentNode.remove();
-    }
-  });
+  
+ 
 });
